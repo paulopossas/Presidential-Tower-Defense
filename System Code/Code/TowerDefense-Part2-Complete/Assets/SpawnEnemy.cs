73cf24dbcf67551e27ab78af782599dbbc23293b
paulopossas <paulopossas@id.uff.br>
2016-04-07 11:50:43 -0600

    Revert "Trump in da haus"
    This reverts commit 2a75697954eb8e0e6a4e98111f2870c1d1b177dc.

--- System Code/Code/TowerDefense-Part2-Complete/Assets/SpawnEnemy.cs
+++ System Code/Code/TowerDefense-Part2-Complete/Assets/SpawnEnemy.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 [System.Serializable]
 public class Wave {
-	public GameObject [] enemyPrefabs;
+	public GameObject enemyPrefab;
 	public float spawnInterval = 2;
-	public int maxEnemies = 20;
+	public int maxEnemies = 10;
 }
 
 public class SpawnEnemy : MonoBehaviour {
@@ -13,7 +13,6 @@ public class SpawnEnemy : MonoBehaviour {
 	public GameObject[] waypoints;
 	public Wave[] waves;
 	public int timeBetweenWaves = 5;
-	public int enemyCount;
 	
 	private GameManagerBehavior gameManager;
 	
@@ -25,7 +24,6 @@ public class SpawnEnemy : MonoBehaviour {
 		lastSpawnTime = Time.time;
 		gameManager =
 			GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
-		
 	}
 	
 	// Update is called once per frame
@@ -37,20 +35,15 @@ public class SpawnEnemy : MonoBehaviour {
 			float timeInterval = Time.time - lastSpawnTime;
 			float spawnInterval = waves[currentWave].spawnInterval;
 			if (((enemiesSpawned == 0 && timeInterval > timeBetweenWaves) ||
-				timeInterval > spawnInterval) && 
-				enemiesSpawned < waves[currentWave].maxEnemies) {
+			     timeInterval > spawnInterval) && 
+			    enemiesSpawned < waves[currentWave].maxEnemies) {
 				// 3  
 				lastSpawnTime = Time.time;
-				GameObject newEnemy = (GameObject)Instantiate(waves[currentWave].enemyPrefabs[enemyCount]);
-				Debug.Log (currentWave);
-				Debug.Log (enemyCount);
-				if (enemyCount < waves [currentWave].enemyPrefabs.Length) {
-					enemyCount++;
-				}
+				GameObject newEnemy = (GameObject)
+					Instantiate(waves[currentWave].enemyPrefab);
 				newEnemy.GetComponent<MoveEnemy>().waypoints = waypoints;
 				enemiesSpawned++;
 			}
-
 			// 4 
 			if (enemiesSpawned == waves[currentWave].maxEnemies &&
 			    GameObject.FindGameObjectWithTag("Enemy") == null) {
