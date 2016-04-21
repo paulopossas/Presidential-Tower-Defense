215586ccff1269c4ab0f9909d6f80c7ac27e9e09
ibstudent220 <ibstudent220@gmail.com>
2016-04-21 13:44:49 -0500

    version from DJ

---
+++ System Code/Code/TowerDefense-Beta/Assets/GameManagerBehavior.cs
@@ -0,0 +1,200 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManagerBehavior : MonoBehaviour {
+
+	public bool isPaused = false;
+	public bool hasMusic;
+	public bool hasSound;
+	public int level;
+
+	public bool isRepublican;
+
+	private GameObject PauseMenu;
+	private int gameSpeed = 1;
+
+	public Text goldLabel;
+	private int gold;
+	public int Gold {
+  		get { return gold; }
+  		set {
+			gold = value;
+    		goldLabel.GetComponent<Text>().text = "GOLD: " + gold;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		Gold = 1000;
+		Wave = 0;
+		Health = 5;
+
+		int party = PlayerPrefs.GetInt ("faction");
+
+		if (party == 1) {
+			isRepublican = false;
+		} else if (party == 2) {
+			isRepublican = true;
+		} else {
+			isRepublican = true;
+		}
+
+
+		// checking if it works
+		if (isRepublican) {
+			print ("Republican party chosen");
+		} else {
+			print ("Democrat party chosen");
+		}
+
+		level = PlayerPrefs.GetInt ("level");
+		//TO DO: do something with the level
+
+		int music = PlayerPrefs.GetInt ("music");
+		int sound = PlayerPrefs.GetInt ("sound");
+
+		print ("has sound: " + sound);
+		print ("has music: " + music);
+
+		hasMusic = (music == 1);
+		hasSound = (sound == 1);
+	}
+
+	// modifies the road and background (and possibly the spawner) to match the given level
+	void loadLevel() {
+
+	}
+
+
+	public void establishPauseMenu(GameObject p)
+	{
+		PauseMenu = p;
+		//print ("Established Pause Menu");
+	}
+
+	public void setSpeed(int x)
+	{
+		if (x > 0) {
+			if (x < 20) {
+				
+				gameSpeed = x;
+
+				if (!isPaused) {
+					Time.timeScale = x;
+				}
+			}
+		}
+	}
+
+	public void pause()
+	{
+		
+
+		Time.timeScale = 0;
+		/*
+		GameObject[] enemys = GameObject.FindGameObjectsWithTag("Enemy");
+		
+		for (int i = 0; i < enemys.Length; ++i) {
+			enemys [i].GetComponent<MoveEnemy> ().pause ();
+		}
+
+		GameObject[] towers = GameObject.FindGameObjectsWithTag("Monster");
+
+		for (int i = 0; i < towers.Length; ++i) {
+			towers [i].GetComponent<ShootEnemies> ().pause ();
+		}
+
+		
+
+		GameObject.Find ("Road").GetComponent<SpawnEnemy> ().pause ();
+
+		*/
+
+		PauseMenu.GetComponentInChildren<PauseMenuScript> ().openMenu ();
+
+		isPaused = true;
+	}
+
+	public void unpause()
+	{
+		/*
+		GameObject[] enemys = GameObject.FindGameObjectsWithTag("Enemy");
+
+		for (int i = 0; i < enemys.Length; ++i) {
+			enemys [i].GetComponent<MoveEnemy> ().unpause ();
+		}
+
+		GameObject[] towers = GameObject.FindGameObjectsWithTag("Monster");
+		for (int i = 0; i < towers.Length; ++i) {
+			towers [i].GetComponent<ShootEnemies> ().unpause ();
+		}
+
+		GameObject.Find ("Road").GetComponent<SpawnEnemy> ().unpause ();
+		*/
+		Time.timeScale = gameSpeed;
+
+		PauseMenu.GetComponentInChildren<PauseMenuScript> ().hideMenu ();
+
+		isPaused = false;
+	}
+
+	public Text waveLabel;
+	public GameObject[] nextWaveLabels;
+
+	public bool gameOver = false;
+
+	private int wave;
+	public int Wave {
+		get { return wave; }
+		set {
+			wave = value;
+			if (!gameOver) {
+				for (int i = 0; i < nextWaveLabels.Length; i++) {
+					nextWaveLabels[i].GetComponent<Animator>().SetTrigger("nextWave");
+				}
+			}
+			waveLabel.text = "WAVE: " + (wave + 1);
+		}
+	}
+
+	public Text healthLabel;
+	public GameObject[] healthIndicator;
+
+	private int health;
+	public int Health {
+		get { return health; }
+		set {
+			// 1
+			if (value < health) {
+				Camera.main.GetComponent<CameraShake>().Shake();
+			}
+			// 2
+			health = value;
+			healthLabel.text = "HEALTH: " + health;
+			// 2
+			if (health <= 0 && !gameOver) {
+				gameOver = true;
+				GameObject gameOverText = GameObject.FindGameObjectWithTag ("GameOver");
+				gameOverText.GetComponent<Animator>().SetBool("gameOver", true);
+			}
+			// 3 
+			for (int i = 0; i < healthIndicator.Length; i++) {
+				if (i < Health) {
+					healthIndicator[i].SetActive(true);
+				} else {
+					healthIndicator[i].SetActive(false);
+				}
+			}
+		}
+	}
+
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
