0b2cdfc7915bfa49e6972f6d43b17ee4747e487b
ibstudent220 <ibstudent220@gmail.com>
2016-04-21 13:43:49 -0500

    version from DJ

---
+++ System Code/Code/TowerDefense-Beta/Assets/BuildScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildScript : MonoBehaviour {
+
+	public GameObject towerPrefab;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUp() {
+
+		//print ("Ok");
+
+		transform.parent.transform.GetComponent<BuildMenuScript> ().attemptBuild(towerPrefab);
+	}
+
+
+}
