f2111eb677c50452c1ce14b0d144aad411bc2849
ibstudent220 <ibstudent220@gmail.com>
2016-04-21 13:47:04 -0500

    version from DJ

---
+++ System Code/Code/TowerDefense-Beta/Assets/Scripts/NavigationBtnScript.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigationBtnScript : MonoBehaviour {
+
+	public GameObject mainMenuBtns;
+	public GameObject factionSelectBtns;
+	public GameObject levelSelectBtns;
+
+	public GameObject helpMenuView;
+
+	public bool sound = true;
+	public bool music = true;
+
+	// this is changed as the user makes decisions about their faction.
+	public int faction = 0; // 1 for democrat, 2 for republican.
+
+
+	void Start()
+	{
+		//helpMenuView = GameObject.FindGameObjectWithTag ("HelpMenu");
+
+	}
+
+	// Use this for initialization
+	void Awake () {
+		mainMenuBtns = GameObject.FindGameObjectWithTag ("MainMenu");
+		factionSelectBtns = GameObject.FindGameObjectWithTag ("SelectFaction");
+		levelSelectBtns = GameObject.FindGameObjectWithTag ("SelectLevel");
+
+		mainMenuBtns.transform.localScale = new Vector3(1,1,1);
+		factionSelectBtns.transform.localScale = new Vector3(0,0,0);
+		levelSelectBtns.transform.localScale = new Vector3(0,0,0);
+
+	}
+
+	public void ChooseFaction(int input)
+	{
+		bool isValid = true;
+		switch (input) {
+		case 1:
+			faction = 1;
+			break;
+		case 2:
+			faction = 2;
+			break;
+		default:
+			isValid = false;
+			break;
+
+		}
+
+		if (isValid) {
+			ToLevelSelect ();
+		}
+	}
+
+	public void ChooseLevel(int input)
+	{
+		int levelChosen = 0;
+		bool isValid = true;
+
+		switch (input) {
+		case 1:
+			levelChosen = input;
+			break;
+		default:
+			isValid = false;
+			break;
+		}
+
+		if (isValid) {
+			StartLevel (levelChosen);
+		}
+	}
+
+	public void StartLevel(int levelChosen)
+	{
+		print ("Move to chosen level");
+		PlayerPrefs.SetInt ("faction", faction);
+		PlayerPrefs.SetInt ("level", levelChosen);
+
+		int hasSound = 0;
+		if (sound) {
+			hasSound = 1;
+		}
+
+		int hasMusic = 0;
+		if (music) {
+			hasMusic = 1;
+		}
+			
+		PlayerPrefs.SetInt ("sound", hasSound);
+		PlayerPrefs.SetInt ("music", hasMusic);
+
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("scenes/GameScene");
+		//Application.Load ("../GameScene.unity");
+	}
+
+	public void ToMainMenu()
+	{
+		mainMenuBtns.transform.localScale = new Vector3(1,1,1);
+		factionSelectBtns.transform.localScale = new Vector3(0,0,0);
+		levelSelectBtns.transform.localScale = new Vector3(0,0,0);
+
+	}
+
+	public void ToSelectFaction()
+	{
+		//print ("Selecting faction");
+
+		mainMenuBtns.transform.localScale = new Vector3(0,0,0);
+		factionSelectBtns.transform.localScale = new Vector3(1,1,1);
+		levelSelectBtns.transform.localScale = new Vector3(0,0,0);
+	}
+
+	public void ToLevelSelect()
+	{
+		mainMenuBtns.transform.localScale = new Vector3(0,0,0);
+		factionSelectBtns.transform.localScale = new Vector3(0,0,0);
+		levelSelectBtns.transform.localScale = new Vector3(1,1,1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+
+	public void toggleSound()
+	{
+		sound = !sound;
+
+		print ("has sound: " + sound);
+
+	}
+
+	public void toggleMusic()
+	{
+		music = !music;
+		print ("has music: " + music);
+	}
+
+	public void toggleHelp()
+	{
+		//what should this menu have?
+		// TODO: Make the menu actually display properly
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ System Code/Code/TowerDefense-Beta/Assets/TowerMenuScript.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityTest.IntegrationTestRunner;
+
+public class TowerMenuScript : MonoBehaviour {
+
+	public string id;
+
+	private GameManagerBehavior gameManager;
+
+	private int counter = 30;
+
+	private Transform upBtn;
+	private Transform canBtn;
+	private Transform ring;
+	private bool menuVisible;
+
+	public GameObject progenitor;
+
+	public bool isReady = false;
+
+	public GameObject monsterPrefab;
+	public GameObject monster;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Awake() {
+
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
+
+		upBtn = transform.parent.transform.FindChild("Upgrade");
+		canBtn = transform.parent.transform.FindChild("Cancel");
+		ring = transform.parent.transform.FindChild("Ring");
+		hideMenu ();
+
+
+
+		//transform.parent.GetComponent<Button>().onClick.AddListener(() => { OnMouseUp(); OnMouseUp(); }); 
+
+	}
+
+	public void setProgenitor(GameObject p)
+	{
+		progenitor = p;
+	}
+
+	public void setID(string nid)
+	{
+		id = nid;
+	}
+
+	public void hideMenu()
+	{
+		//print ("hiding menu");
+		upBtn.localScale = new Vector3 (0,0,0);
+		canBtn.localScale = new Vector3 (0,0,0);
+		ring.localScale = new Vector3 (0,0,0);
+		menuVisible = false;
+
+		upBtn.gameObject.GetComponent<CircleCollider2D> ().radius = 0;
+		canBtn.gameObject.GetComponent<CircleCollider2D> ().radius = 0;
+
+		// collider
+		//transform.parent.transform.FindChild ("Collider").gameObject.GetComponent<CircleCollider2D> ().radius = 0.54f;
+
+		if (isReady) {
+			//print ("Ready");
+			progenitor.GetComponent<BuildMenuScript>().showSelf();
+		} else {
+			//print ("not ready");
+			isReady = true;
+		}
+	}
+
+	public void ShowMenu()
+	{
+		//print ("Menu clicked");
+		upBtn.localScale = new Vector3 (1,1,1);
+		canBtn.localScale = new Vector3 (1,1,1);
+		ring.localScale = new Vector3 (1,1,1);
+
+		upBtn.gameObject.GetComponent<CircleCollider2D> ().radius = 0.4f;
+		canBtn.gameObject.GetComponent<CircleCollider2D> ().radius = 0.4f;
+		// collider
+		//transform.parent.transform.FindChild ("Collider").gameObject.GetComponent<CircleCollider2D> ().radius = 0;
+
+
+		menuVisible = true;
+
+		progenitor.GetComponent<BuildMenuScript> ().hideSelf ();
+	}
+
+	public void setMonster(GameObject m)
+	{
+		monster = m;
+	}
+
+	public bool hasMonster()
+	{
+		return (monster == null);
+	}
+
+	void OnMouseUp()
+	{
+		if (!hasMonster ()) {
+			// bring up menu to build tower
+		} else {
+
+			if (!menuVisible) {
+				ShowMenu ();
+			}
+
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		if (counter <= 0) {
+			counter = 200;
+
+		}
+		if ((counter % 10 == 0)) {
+
+		}
+
+		--counter;
+	}
+
+	private bool canUpgradeMonster() {
+
+		if (hasMonster()) {
+		} else {
+			MonsterData monsterData = monster.GetComponent<MonsterData> ();
+			MonsterLevel nextLevel = monsterData.getNextLevel ();
+			if (nextLevel != null) {
+				return gameManager.Gold >= nextLevel.cost;
+			} else {
+				hideMenu ();
+			}
+		}
+
+
+		return false;
+	}
+
+	private void upgradeMonster()
+	{
+		if (canUpgradeMonster ()) {
+
+			monster.GetComponent<MonsterData> ().increaseLevel ();
+			//AudioSource audioSource = gameObject.GetComponent<AudioSource> ();
+			//audioSource.PlayOneShot (audioSource.clip);
+
+			gameManager.Gold -= monster.GetComponent<MonsterData> ().CurrentLevel.cost;
+		} else {
+		}
+	}
+
+
+	public void upgrade()
+	{
+		// call upgradeMonster in parent
+		if (canUpgradeMonster ()) {
+			upgradeMonster ();
+
+			hideMenu ();
+		}
+
+
+	}
+
+	public void cancel()
+	{
+		hideMenu ();
+	}
+}
